2b15a31f19ec46805dbaacc0d2cfdb1db7c4296b
Linda Chen <Linda@Lindas-MacBook-Pro.local>
2017-10-17 13:41:04 -0400

    WeatherGift

--- WeatherGift/ListVC.swift
+++ WeatherGift/ListVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ListVC.swift
 //  WeatherGift
 //
 //  Created by Linda Chen on 10/17/17.
@@ -8,18 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ListVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
 
 }
-
---
+++ WeatherGift/PageVC.swift
@@ -0,0 +1,35 @@
+//
+//  PageVC.swift
+//  WeatherGift
+//
+//  Created by Linda Chen on 10/17/17.
+//  Copyright © 2017 Synestha. All rights reserved.
+//
+
+import UIKit
+
+class PageVC: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
