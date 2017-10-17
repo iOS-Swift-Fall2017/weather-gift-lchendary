078b71c926a8c5d89371ccc5de93e5a61e06eece
Linda Chen <Linda@Lindas-MacBook-Pro.local>
2017-10-17 14:55:59 -0400

    WeatherGift

--- WeatherGift/DetailVC.swift
+++ WeatherGift/DetailVC.swift
@@ -9,27 +9,21 @@
 import UIKit
 
 class DetailVC: UIViewController {
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var summaryLabel: UILabel!
+    @IBOutlet weak var currentImage: UIImageView!
+    
+    var currentPage = 0
+    var locationsArray = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        locationLabel.text = locationsArray[currentPage]
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- WeatherGift/PageVC.swift
+++ WeatherGift/PageVC.swift
@@ -9,27 +9,82 @@
 import UIKit
 
 class PageVC: UIPageViewController {
+    
+    var currentPage = 0
+    var locationsArray = ["Local City", "Chestnut Hill, MA", "New York, NY"]
+    
+    var pageControl: UIPageControl!
+    var barButtonWidth: CGFloat = 44
+    var barButtonHeight: CGFloat = 44
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        delegate = self
+        dataSource = self
+        
+        setViewControllers([createDetailVC(forPage: 0)], direction: .forward, animated: false, completion: nil)
+   
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        configurePageControl()
     }
-    */
+    
+    //Page control
+    func configurePageControl() {
+        let pageControlHeight = barButtonWidth
+        let pageControlWidth: CGFloat = view.frame.width - (barButtonWidth * 2)
+        
+        pageControl = UIPageControl(frame: CGRect(x: (view.frame.width - pageControlWidth) / 2, y: view.frame.height - pageControlHeight, width: pageControlWidth, height: pageControlHeight))
+        pageControl.pageIndicatorTintColor = UIColor.lightGray
+        pageControl.currentPageIndicatorTintColor = UIColor.black
+        pageControl.numberOfPages = locationsArray.count
+        pageControl.currentPage = currentPage
+       
+        view.addSubview(pageControl)
+    }
+    
+    // MARK: - Create view controller for UIPageViewController
+    func createDetailVC(forPage page: Int) -> DetailVC {
+        
+        currentPage = min(max(0, page), locationsArray.count-1)
+        let detailVC = storyboard!.instantiateViewController(withIdentifier: "DetailVC") as! DetailVC
+        
+        detailVC.locationsArray = locationsArray
+        detailVC.currentPage = currentPage
+        
+        return detailVC
+    }
+    
+}
 
+extension PageVC: UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        
+        if let currentViewController = viewController as? DetailVC {
+            if currentViewController.currentPage < locationsArray.count-1 {
+                return createDetailVC(forPage: currentViewController.currentPage + 1)
+            }
+        }
+        return nil
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        if let currentViewController = viewController as? DetailVC {
+            if currentViewController.currentPage > 0 {
+                return createDetailVC(forPage: currentViewController.currentPage - 1)
+            }
+        }
+        return nil
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        if let currentViewController = pageViewController.viewControllers?[0] as? DetailVC {
+            pageControl.currentPage = currentViewController.currentPage
+        }
+    }
+    
 }
